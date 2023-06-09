2bb640b3d2063911888ae99a7f29ec78e497773f
Jean-Luc <jeanlucv@berkeley.edu>
2023-06-08 21:00:17 -0700

    Create Button and reset button work

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,42 @@
-
-
-
-
-
-
-
-
-
-
-
-// <div>response.distance</div>
-// <div>response.age</div>
-// <div>reponse.course</div>
-
-
-
-
-
+async function fetchRandomHorse() {
+    const url = 'https://horse-racing-usa.p.rapidapi.com/results?date=2021-03-18';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd2eeb2e5e5msh326a9c42c5a980dp149abfjsnffe7aa302f90',
+        'X-RapidAPI-Host': 'horse-racing-usa.p.rapidapi.com'
+      }
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+  
+      // Generate a random index within the range of horses in API
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomHorse = data[randomIndex];
+  
+      const { age, course, distance } = randomHorse;
+      console.log('Age:', age);
+      console.log('Course:', course);
+      console.log('Distance:', distance);
+  
+      var HorsestatsHTML = `
+        <h3>
+          <div>
+            <div>  
+              <div>${age}</div>
+              <div>${course}</div>
+              <div>${distance}</div>
+          </div>
+        </h3>`;
+  
+      $(".GenerateStats").html(HorsestatsHTML);
+  
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
 
@@ -31,17 +51,17 @@
 
 
 $("#clearstorage").on("click", function(event) {
-    localStorage.clear();
-    console.log("Clearstorage Button clicked!");
+localStorage.clear();
+console.log("Clearstorage Button clicked!");
 });
 
 $("#CreateFighters").on("click", function(event) {
-
+fetchRandomHorse()
 console.log("Createfighters Button clicked!"); 
 });
 
 $("#ResetBattle").on("click", function(event){
-
+    $(".GenerateStats").html('')
 console.log("ResetBattle Button clicked!");
 });
 
