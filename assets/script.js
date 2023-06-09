620d21e972621bb37c074b7805d7baba83f9f493
Jean-Luc <jeanlucv@berkeley.edu>
2023-06-08 19:28:29 -0700

    Javascript button work

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+// <div>response.distance</div>
+// <div>response.age</div>
+// <div>reponse.course</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("#clearstorage").on("click", function(event) {
+    localStorage.clear();
+    console.log("Clearstorage Button clicked!");
+});
+
+$("#CreateFighters").on("click", function(event) {
+
+console.log("Createfighters Button clicked!"); 
+});
+
+$("#ResetBattle").on("click", function(event){
+
+console.log("ResetBattle Button clicked!");
+});
+
+$("#FightersBattle").on("click", function(event){
+    console.log("ResetBattle Button clicked!")  
+});
+
+$("#previousfights").on("click", function(event){
+console.log("Previousfight button clicked!")
+});
