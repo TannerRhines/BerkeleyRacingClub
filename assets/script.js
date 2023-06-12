9867f744a316502fd8fcb167e9cad9503e35378d
Zach Antunes <ztunes4@gmail.com>
2023-06-12 13:42:59 -0700

    finished the Create! and Battle! buttons. Create! now looks at the horse stats and determines which horse is the winner, Battle! starts the animation and displays the winner.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
-async function fetchRandomHorse(targetClass, horseName) {
+let horse1Data = {};
+let horse2Data = {};
+
+async function fetchRandomHorse(targetClass, horseName, horseData) {
     const url = 'https://horse-racing-usa.p.rapidapi.com/results?date=2021-03-18';
     const options = {
       method: 'GET',
@@ -7,24 +10,27 @@ async function fetchRandomHorse(targetClass, horseName) {
         'X-RapidAPI-Host': 'horse-racing-usa.p.rapidapi.com'
       }
     };
-  
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-  
-      // Generate a random index within the range of horses in API
-      const randomIndex = Math.floor(Math.random() * data.length);
-      const randomHorse = data[randomIndex];
-  
-      const { course, distance } = randomHorse;
-      // age conversion function turns "age" into a number we can use to compare horses
-      let age = randomHorse.age;
-      age = ageConversion(age); 
-      let convertedDistance = distanceConversion(distance);
 
-      console.log('Age:', age);
-      console.log('Course:', course);
-      console.log('Distance:', distance);
+let age;
+let convertedDistance;
+
+try {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  // Generate a random index within the range of horses in API
+  const randomIndex = Math.floor(Math.random() * data.length);
+  const randomHorse = data[randomIndex];
+
+  const { course, distance } = randomHorse;
+  // age conversion function turns "age" into a number we can use to compare horses
+  age = randomHorse.age;
+  age = ageConversion(age); 
+  convertedDistance = distanceConversion(distance);
+
+  console.log('Age:', age);
+  console.log('Course:', course);
+  console.log('Distance:', distance);
   
       var HorsestatsHTML = `
   <h3>
@@ -44,7 +50,10 @@ async function fetchRandomHorse(targetClass, horseName) {
     } catch (error) {
       console.error(error);
     }
-  };
+
+    horseData.age = age;
+    horseData.convertedDistance = convertedDistance;
+};
 
   // converts age string into a number, removes the "yo" and "yo+", turns "yo+" into .5, so 3yo+ = 3.5
 
@@ -58,27 +67,57 @@ async function fetchRandomHorse(targetClass, horseName) {
     return ageValue;
 }
 
+// converts furlongs, X miles Y yards into miles, returns value as XYZ miles
+
 function distanceConversion(distanceString) {
   const yardToMiles = 1 / 1760;
   const furlongToMiles = 1 / 8;
   let distanceInMiles;
 
   // check for the various formats
+
+  // if api returns furlongs
   if (distanceString.includes('furlong')) {
     let furlongValue = parseFloat(distanceString.replace('furlongs', '').replace('furlong', ''));
     distanceInMiles = furlongValue * furlongToMiles;
-  } else if (distanceString.includes('mile') && distanceString.includes('yard')) {
+  } 
+  
+  // if API returns miles + yards
+  else if (distanceString.includes('mile') && distanceString.includes('yard')) {
     let [miles, yards] = distanceString.split(' ');
     miles = parseFloat(miles.replace('mile', ''));
     yards = parseFloat(yards.replace('yards', '')) * yardToMiles;
     distanceInMiles = miles + yards;
-  } else if (distanceString.includes('mile')) {
+  } else if 
+  
+  // if API returns miles
+  (distanceString.includes('mile')) {
     distanceInMiles = parseFloat(distanceString.replace('miles', '').replace('mile', ''));
   }
 
   // rounding to the nearest hundredth
   return Math.round(distanceInMiles * 100) / 100;
-}
+};
+
+// determines which horse won the race
+
+function horseBattle () {
+  var raceValueHorse1 = (Math.abs(horse1Data.age - 3.5)) + (Math.abs(horse1Data.convertedDistance - 1.25));
+
+  var raceValueHorse2 = (Math.abs(horse2Data.age - 3.5)) + (Math.abs(horse2Data.convertedDistance - 1.25));
+
+  if (raceValueHorse1 < raceValueHorse2) {
+    console.log("Horse 1 wins!");
+    $('#race-results').text("Horse 1 wins!");
+  } else if (raceValueHorse2 < raceValueHorse1) {
+    console.log("Horse 2 wins!");
+    $('#race-results').text("Horse 2 wins!");
+  } else {
+    console.log("It's a tie!");
+    $('#race-results').text("It's a tie!");
+  }
+
+};
 
 
 async function fetchRandomPerson() {
@@ -136,37 +175,54 @@ attachImages();
 // take age, determine absolute value from 3.5, take distance, take determine absolute value from 1.25 miles, add numbers together, whichever horse has lowest number wins
 
 
+
+
 $("#clearstorage").on("click", function(event) {
 localStorage.clear();
 console.log("Clearstorage Button clicked!");
 });
 
-// click listener works with API, commenting out for now to save API calls
+// Create! button calls the fetchRandomPerson and fetchRandomHorse APIs, stores their race stats to horse1data and horse2data, then horseBattl(); uses a promise to wait for the APIs to finish pulling in the data, then determines which horse wins before Battle! button is clicked
 
- $("#CreateFighters").on("click", function(event) {
-  fetchRandomPerson()
+$("#CreateFighters").on("click", function(event) {
+  let horse1Promise = fetchRandomPerson()
     .then(name => {
-      return fetchRandomHorse(".GenerateStats1", name);
+      return fetchRandomHorse(".Horse-1", name, horse1Data);
+    })
+    .then(() => {
+      console.log('Horse 1 data:', horse1Data);
     })
     .catch(error => {
       console.error('Error:', error);
     });
     
-  fetchRandomPerson()
+  let horse2Promise = fetchRandomPerson()
     .then(name => {
-      return fetchRandomHorse(".GenerateStats2", name);
+      return fetchRandomHorse(".Horse-2", name, horse2Data);
+    })
+    .then(() => {
+      console.log('Horse 2 data:', horse2Data);
     })
     .catch(error => {
       console.error('Error:', error);
     });
-    
+  
+  Promise.all([horse1Promise, horse2Promise])
+    .then(() => {
+      horseBattle();
+    });
+
   console.log("Createfighters Button clicked!"); 
 });
 
 
+
+
+
 $("#ResetBattle").on("click", function(event){
-    $(".GenerateStats1").html('');
-    $(".GenerateStats2").html('');
+    $(".Horse-1").html('');
+    $(".Horse-2").html('');
+    $("#race-results").html('');
 console.log("ResetBattle Button clicked!");
 });
 
@@ -181,6 +237,10 @@ $("#FightersBattle").on("click", function(event){
     $('#image-1 img').removeClass('battle1');
     $('#image-2 img').removeClass('battle2');
   }, 2000);  // The duration of the animations in milliseconds
+
+  setTimeout(function() {
+    $('#race-results').removeClass('hidden');
+  }, 2500)
 });
 
 $("#previousfights").on("click", function(event){
